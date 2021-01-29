579079ae8fa0b7b9763ebc44b6f63dd4f197539a
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-01-28 21:24:21 -0600

    Done.

---
+++ apps/models/show.rb
@@ -0,0 +1,27 @@
+class Show < ActiveRecord::Base 
+
+    # def highest_rating
+    #     Show.rating.maximum
+    # end
+
+    # def most_popular_show
+        # Show.order(rating: :desc).first
+    # end
+
+    # def lowest_rating
+        #Show.rating.minimum
+    # end
+
+    # def least_popular_show
+    # end
+
+    # def ratings_sum
+    # end
+
+    # def popular_shows
+    # end
+
+    # def shows_by_alphabetical_order
+    # end
+
+end
